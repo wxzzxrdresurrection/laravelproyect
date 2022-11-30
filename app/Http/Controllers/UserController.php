c3e94cfb9828e271ecc01488c70ff3d2c0c19310
wxzzxrdresurrection <magicwizz12@gmail.com>
2022-11-30 16:58:38 -0600

    CREACION DE MIGRACIONES Y CONTROLADOR USUARIO

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function registrarUsuario(Request $request){
+
+    }
+
+    public function verificarCorreo(Request $request){
+
+    }
+
+    public function enviarSMS(Request $request){
+
+    }
+
+    public function verificarSMS(Request $request){
+        
+    }
+
+    public function login(Request $request){
+
+    }
+
+    public function logout(Request $request){
+
+    }
+}
