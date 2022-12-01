d22604d15daf2a7336e7653e792b7affe7403f1d
wizzard <magicwizz12@gmail.com>
2022-12-01 10:50:41 -0600

    CONTROLADOR ADAFRUIT

---
+++ app/Http/Controllers/AdafruitController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdafruitController extends Controller
+{
+    public function infrarrojoLectura(Request $request){
+
+    }
+
+    public function lluviaLectura(Request $request){
+        
+    }
+
+    public function pesoLectura(Request $request){
+        
+    }
+
+    public function iluminacionLectura(Request $request){
+        
+    }
+
+    public function aguaLectura(Request $request){
+        
+    }
+
+    public function comidaLectura(Request $request){
+        
+    }
+}
