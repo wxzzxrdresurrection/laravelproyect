55a4fa98ad3306f37cd22515b4ff4dde529556fa
Javier Resendiz <javier.res220704@gmail.com>
2022-12-12 11:11:53 -0600

    Seeder admin

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            "nombre"=>"Admin",
+            "ap_paterno"=>"Admin",
+            "ap_materno"=>"Admin",
+            "correo"=>"javier.res220704@gmail.com",
+            "telefono"=>"8713814026",
+            "password"=>"J22r07c04"
+        ]);
+    }
+}
